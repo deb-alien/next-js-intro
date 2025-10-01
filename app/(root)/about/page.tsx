da4018ca2d5cfb9974e546cfd31029a8e9170277
deb-alien <DevPro-2011@outlook.com>
2025-10-02 00:24:05 +0600

    Add local Error and Global error handler and add a loader component

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,4 +1,5 @@
 export default function About() {
+    throw new Error('Not Implemented')
     return (
         <>
             <h1>About</h1>
---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+	return (
+		<div>
+			<h1>__________</h1>
+		</div>
+	);
+}
